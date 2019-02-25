64f59a0894e39ed431f5fd5aa56ea78b0dc7d6c2
MayraPre <mayrapresolar@gmail.com>
2019-02-25 11:16:36 -0500

    subiendo parte 1

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,30 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, string) => {
+    let resultado = "";
+    for (let i = 0; i < string.length; i++)
+      let posicion = string.charCodeAt(i);
+      if (posicion > 64 && posicion <= 90) {
+        resultado += String.fromCharCode(((posicion - 65 + Math.abs(offset)) % 26) + 65);
+      }
+      else if (posicion == 32) {
+        resultado += " ";
+      }
+    }
+    return (resultado)
+    /* Acá v  a tu código */
   },
-  decode: () => {
+  decode: (offset, string) => {
+    let resultado = "";
+		for (let i = 0; i < string.length; i++)
+			let posicion = string.charCodeAt(i);
+			if (posicion > 64 && posicion <= 90) {
+				resultado += String.fromCharCode((posicion - 90 - offset) % 26 + 90);
+
+			}
+			else if (posicion == 32) {
+				resultado += " ";
+			}
+		
+		return (resultado)
     /* Acá va tu código */
-  }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-/* Acá va tu código */
+/* Acá va tu   código */
+btnEncode = document.getElementById("buttonEncode");
+btnEncode.addEventListener("click", () => {
+	string = document.getElementById("txt_ingresado").value.toUpperCase();
+	offset = parseInt(document.getElementById("txt_offset").value);
+	resultado = cipher.encode(offset,string);
+	document.getElementById("txt_resultado").innerHTML = resultado;
+ })
+
+  btnDecode = document.getElementById("buttonDecode")
+  btnDecode.addEventListener("click", function () {
+  	string = document.getElementById("txt_ingresado").value;
+  	offset = parseInt(document.getElementById("txt_offset").value);
+  	resultado = cipher.decode(offset, string);
+  	document.getElementById("txt_resultado").innerHTML = resultado;
+  });
